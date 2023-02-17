c3394f20ee933398bb863048f0330d37b5b3181f
z990814 <67317862+z990814@users.noreply.github.com>
2023-02-17 11:47:18 +0800

    新建页面

---
+++ src/components/PieChart.vue
@@ -0,0 +1,9 @@
+<!--饼图-->
+<template></template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/RingChart.vue
@@ -0,0 +1,9 @@
+<!--环图-->
+<template></template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
